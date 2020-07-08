5640548bf89c463b5c9e979543fa79fcaeace8a2
Cássio <cassiocappellari@gmail.com>
2020-07-08 17:16:28 -0300

    css and links updated

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const routes = require('./routes')
 const server = express()
 
 server.use(express.static('public'))
+server.use(routes)
+
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
