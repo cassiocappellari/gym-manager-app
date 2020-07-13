56123b9aa5d47768fff05b49b27482b55a748006
Cássio <cassiocappellari@gmail.com>
2020-07-13 13:41:10 -0300

    organized data

--- instructors.js
+++ instructors.js
@@ -1,4 +1,5 @@
-// create
+const fs = require('fs')
+const data = require('./data.json')
 
 exports.post = function(req, res) {
 
@@ -9,11 +10,28 @@ exports.post = function(req, res) {
            return res.send('Please, fill all the fields!')
         }
     }
+    
+    let {avatar_url, name, birth, gender, services} = req.body
 
-    res.send(req.body)
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error!')
+
+        return res.redirect('/instructors')
+    })
+
+    //res.send(req.body)
 }
-
-// update
-
-
-// delete
