74e977685175c9fff73c14d8338a895823416d74
Cássio <cassiocappellari@gmail.com>
2020-07-15 15:33:27 -0300

    instructor details done

--- instructors.js
+++ instructors.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const data = require('./data.json')
 const {age} = require('./useful')
+Intl = require('intl')
 
 exports.show = function(req, res) {
     const {id} = req.params
@@ -17,7 +18,7 @@ exports.show = function(req, res) {
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(','),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
     }
 
     return res.render('instructors/show', {instructor})
