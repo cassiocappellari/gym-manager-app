8c28fa99aa4594824985925c76340a7167455f70
Cássio <cassiocappellari@gmail.com>
2020-07-16 15:04:38 -0300

    date of birth bug fixed

--- useful.js
+++ useful.js
@@ -18,8 +18,8 @@ module.exports = {
         const date = new Date(timestamp)
 
         const year = date.getUTCFullYear()
-        const month = `${date.getUTCMonth() + 1}`.slice(-2)
-        const day = `${date.getUTCDate()}`.slice(-2)
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
 
         return `${year}-${month}-${day}`
     }
