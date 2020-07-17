c9afc8d902289172e180d32fe8f1bbe1e7b4182a
Cássio <cassiocappellari@gmail.com>
2020-07-17 18:27:35 -0300

    feature delete done

--- instructors.js
+++ instructors.js
@@ -75,3 +75,54 @@ exports.edit = function(req, res) {
 
     return res.render('instructors/edit', {instructor})
 }
+
+exports.put = function(req, res) {
+    const {id} = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){ // 
+        if(id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundInstructor) {
+        return res.send('Instructor not founded!')
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+        if(err) {
+            return res.send('Error!')
+        }
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+exports.delete = function(req, res) {
+    const {id} = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) {
+            return res.send('Error!')
+        }
+    })
+
+    return res.redirect('/instructors')
+}
--- routes.js
+++ routes.js
@@ -20,6 +20,10 @@ routes.get('/instructors/:id/edit', instructors.edit)
 
 routes.post('/instructors', instructors.post)
 
+routes.put('/instructors', instructors.put)
+
+routes.delete('/instructors', instructors.delete)
+
 routes.get('/members', function(req, res) {
     return res.send('members')
 })
