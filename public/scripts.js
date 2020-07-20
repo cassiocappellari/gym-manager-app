0c1548a1a5ab0d6632ac3479a0be5eaab5bbcd3f
Cássio <cassiocappellari@gmail.com>
2020-07-20 10:49:28 -0300

    create members page done

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,9 @@
-const currentPage = location.pathname // 'window.location' separa a localização atual (página) que estamos; 'location.pathname' separa a url da página atual que estamos
+const currentPage = location.pathname
 const menuItens = document.querySelectorAll('header .links a')
 
 for (let item of menuItens) {
-    if(currentPage.includes(item.getAttribute('href'))) { // retorna true quando a função includes() encontra uma string dentro do 'href'
+    if(currentPage.includes(item.getAttribute('href'))) {
         item.classList.add('active')
     }
 }
+
