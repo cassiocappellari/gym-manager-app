3041f2973c4c79e9ea5d89038f29e2f62af8bcd3
Cássio <cassiocappellari@gmail.com>
2020-08-05 20:05:09 -0300

    database configured

---
+++ src/app/controllers/members.js
@@ -0,0 +1,44 @@
+const {age, date} = require('../../lib/useful')
+Intl = require('intl')
+
+module.exports = {
+    index(req, res){
+        return res.render('members/index')
+    },
+    create(req, res){
+        return res.render('members/create')
+    },
+    post(req, res){
+        const keys = Object.keys(req.body)
+
+        for(let key of keys) {
+            if(req.body[key] == '') {
+               return res.send('Please, fill all the fields!')
+            }
+        }
+    
+        return
+    },
+    show(req, res){
+        return
+    },
+    edit(req, res){
+        return
+    },
+    put(req, res){
+        const keys = Object.keys(req.body)
+
+        for(let key of keys) {
+            if(req.body[key] == '') {
+               return res.send('Please, fill all the fields!')
+            }
+        }
+        
+        let {avatar_url, name, birth, gender, services} = req.body
+        
+        return
+    },
+    delete(req, res){
+        return
+    }
+}
--- src/lib/useful.js
+++ src/lib/useful.js
@@ -1,5 +1,5 @@
 module.exports = {
-    age: function(timestamp) {
+    age(timestamp) {
     
         const today = new Date()
         const birthDate = new Date(timestamp)
@@ -14,7 +14,7 @@ module.exports = {
     
         return age
     },
-    date: function(timestamp) {
+    date(timestamp) {
         const date = new Date(timestamp)
 
         const year = date.getUTCFullYear()
