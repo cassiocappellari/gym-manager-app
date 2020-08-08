76bc72b4d55441215d7210e0b694f4228b594bee
Cássio <cassiocappellari@gmail.com>
2020-08-08 12:39:40 -0300

    filter option done

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -4,9 +4,18 @@ Intl = require('intl')
 
 module.exports = {
     index(req, res){
-        Instructor.all(function(instructors) {
-            return res.render('instructors/index', {instructors})
-        })
+        const {filter} = req.query
+
+        if(filter) {
+            Instructor.findBy(filter, function(instructors){
+                return res.render('instructors/index', {instructors, filter})
+            })
+        } else {
+            Instructor.all(function(instructors) {
+                return res.render('instructors/index', {instructors})
+            })
+        }
+
     },
     create(req, res){
         return res.render('instructors/create')
