cae12689132cb81c1c134a5c3fce52eff274a03b
Cássio <cassiocappellari@gmail.com>
2020-08-08 10:27:29 -0300

    show students instructors done

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -45,7 +45,10 @@ module.exports = {
 
             member.birth = date(member.birth).iso
 
-            return res.render('members/edit', {member})
+            Member.instructorsSelectOptions(function(options){
+                return res.render('members/edit', {member, instructorOptions: options})
+            })
+            
         })
     },
     put(req, res){
--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -19,8 +19,9 @@ module.exports = {
                 birth,
                 blood,
                 weight,
-                height
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                height,
+                instructor_id
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING id
         `
 
@@ -32,9 +33,10 @@ module.exports = {
             date(data.birth).iso,
             data.blood,
             data.weight,
-            data.height
+            data.height,
+            data.instructor
         ]
-    
+
         db.query(query, values, function(err, results){
             if(err) throw `Database error! ${err}`
 
@@ -43,9 +45,10 @@ module.exports = {
     },
     find(id, callback) {
         db.query(`
-            SELECT *
+            SELECT members.*, instructors.name AS instructor_name
             FROM members
-            WHERE id = $1`, [id], function(err, results) {
+            LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+            WHERE members.id = $1`, [id], function(err, results) {
                 if(err) throw `Database error! ${err}`
 
                 callback(results.rows[0])
@@ -62,8 +65,9 @@ module.exports = {
             email=($5),
             blood=($6),
             weight=($7),
-            height=($8)
-        WHERE id = $9
+            height=($8),
+            instructor_id=($9)
+        WHERE id = $10
         `
 
         const values = [
@@ -75,6 +79,7 @@ module.exports = {
             data.blood,
             data.weight,
             data.height,
+            data.instructor,
             data.id
         ]
 
