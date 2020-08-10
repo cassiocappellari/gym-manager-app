c585eb0c783f976b38f4e464a6fff0cdf2532bbf
Cássio <cassiocappellari@gmail.com>
2020-08-10 16:57:37 -0300

    gym manager paginate done

--- public/scripts.js
+++ public/scripts.js
@@ -45,19 +45,31 @@ function paginate(selectedPage, totalPages) {
     return pages
 }
 
-const pagination = document.querySelector('.pagination')
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
 
-let elements = ''
+    let elements = ''
 
-for (let page of pages) {
-    if(String(page).includes('...')) {
-        elements += `<span>${page}</span>`
-    } else {
-        elements += `<a href="?page=${page}">${page}</a>`
+    for (let page of pages) {
+        if(String(page).includes('...')) {
+            elements += `<span>${page}</span>`
+        } else {
+            if(filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
     }
+
+    pagination.innerHTML = elements
 }
 
-pagination.innerHTML = elements
+const pagination = document.querySelector('.pagination')
+
+if (pagination) {
+    createPagination(pagination)
+}
