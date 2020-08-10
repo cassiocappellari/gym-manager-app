50cea95dc17a76df526f1da79e81916624f4d194
Cássio <cassiocappellari@gmail.com>
2020-08-10 10:28:11 -0300

    paginate algorithm done

--- public/scripts.js
+++ public/scripts.js
@@ -1,16 +1,45 @@
-const currentPage = location.pathname
+const currentPath = location.pathname
 const menuItens = document.querySelectorAll('header .links a')
 const formDelete = document.querySelector('#form-delete')
 
 for (let item of menuItens) {
-    if(currentPage.includes(item.getAttribute('href'))) {
+    if(currentPath.includes(item.getAttribute('href'))) {
         item.classList.add('active')
     }
 }
 
-formDelete.addEventListener('submit', function(event){
-    const deleteOption = confirm('Are you sure?')
-    if(!deleteOption) {
-        event.preventDefault()
+if(formDelete) {
+    formDelete.addEventListener('submit', function(event){
+        const deleteOption = confirm('Are you sure?')
+        if(!deleteOption) {
+            event.preventDefault()
+        }
+    })
+}
+
+let totalPages = 20,
+    selectedPage = 15,
+    pages = [],
+    oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+    
+    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+        if(oldPage && currentPage - oldPage > 2) {
+            pages.push('...')
+        }
+
+        if(oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+        }
+
+        pages.push(currentPage)
+        oldPage = currentPage
+
     }
-})
+    
+}
